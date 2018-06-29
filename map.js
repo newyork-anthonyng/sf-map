a34f8bf92552e6e146eefc50c620f7edcc3ed6d0
Anthony Ng <a-anng@expedia.com>
2018-06-29 13:20:06 -0700

    Initial commit 🎉

---
+++ map.js
@@ -0,0 +1,60 @@
+// FRONT, LEFT, RIGHT, BACK
+const DIRECTIONS = {
+  FRONT: "FRONT",
+  LEFT: "LEFT",
+  RIGHT: "RIGHT",
+  BACK: "BACK"
+};
+
+const thirdFloor = {
+  alfred: {
+    directions: [DIRECTIONS.FRONT],
+    floor: 3,
+    name: "Alfred"
+  },
+  alma: {
+    directions: [DIRECTIONS.LEFT],
+    floor: 3,
+    name: "Alma"
+  },
+  vertigo: {
+    directions: [DIRECTIONS.LEFT, DIRECTIONS.LEFT],
+    floor: 3,
+    name: "Vertigo"
+  },
+  dialm: {
+    directions: [DIRECTIONS.LEFT, DIRECTIONS.LEFT],
+    floor: 3,
+    name: "Dial M"
+  },
+  gracekelly: {
+    directions: [DIRECTIONS.LEFT, DIRECTIONS.LEFT],
+    floor: 3,
+    name: "Grace Kelly"
+  },
+  wellness: {
+    directions: [DIRECTIONS.LEFT, DIRECTIONS.LEFT],
+    floor: 3,
+    name: "Wellness"
+  },
+  ladyvanishes: {
+    directions: [DIRECTIONS.LEFT, DIRECTIONS.LEFT],
+    floor: 3,
+    name: "Lady Vanishes"
+  },
+  rearwindow: {
+    directions: [DIRECTIONS.RIGHT, DIRECTIONS.RIGHT],
+    floor: 3,
+    name: "Rear Window"
+  },
+  batesmotel: {
+    directions: [DIRECTIONS.RIGHT, DIRECTIONS.RIGHT],
+    floor: 3,
+    name: "Bates Motel"
+  },
+  nbynw: {
+    directions: [DIRECTIONS.RIGHT, DIRECTIONS.RIGHT],
+    floor: 3,
+    name: "N by NW"
+  }
+};
